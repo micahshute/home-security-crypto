375a14eeae70c438a2423564a211f402d10bc2bd
micahshute <micah.shute@gmail.com>
2021-11-07 20:16:18 -0800

    removes print statements

--- OTPStreamCipherReceiver.h
+++ OTPStreamCipherReceiver.h
@@ -46,12 +46,6 @@ void MSCrypto::OTPStreamCipherReceiver<MType, MSize, CType, CSize>::setupCipher(
     this->isSecure = true;
     this->prng = CTrivium(key, iv, true);
     this->streamByteLocation = 0;
-    Serial.print("Setting up myself at ref: ");
-    Serial.println((unsigned long)this);
-    Serial.print("my streambyteloc: ");
-    Serial.print(this->streamByteLocation);
-    Serial.print(" with ref: ");
-    Serial.println((unsigned long) &streamByteLocation);
 };
 
 template <typename MType, size_t MSize, typename CType, size_t CSize>
@@ -64,24 +58,12 @@ uint8_t MSCrypto::OTPStreamCipherReceiver<MType, MSize, CType, CSize>::getRandom
 
 template <typename MType, size_t MSize, typename CType, size_t CSize>
 MType MSCrypto::OTPStreamCipherReceiver<MType, MSize, CType, CSize>::parseMessage(uint64_t fullMessage){
-    // Serial.println("in ParseMessage OTPSCR");
-    // Serial.println((uint32_t)fullMessage);
-    // Serial.print("StreamByteLocation: ");
-    // Serial.println(streamByteLocation);
     CType maxMessageCountNum = (CType)(pow(2, 8*CSize) - 1);
     CType messageCountBytes = fullMessage & maxMessageCountNum;
     MType encodedMessage = fullMessage >> (8 * CSize);
 
     CType missedMessages = MSCrypto::rolloverDifference<CType>(messageCountBytes, streamByteLocation);
     uint16_t maxMissedMessages = 15 * MSize;
-
-    Serial.println("ref; Missed ; streamByteLoc; sblref; maxmissed; msgcount");
-    Serial.println((unsigned long)this);
-    Serial.println((int)missedMessages);
-    Serial.println(this->streamByteLocation);
-    Serial.println((unsigned long) &streamByteLocation);
-    Serial.println((int)maxMissedMessages);
-    Serial.println(messageCountBytes);
 
     if(missedMessages > maxMissedMessages || messageCountBytes % MSize != 0){
         return 0;
@@ -112,7 +94,5 @@ void MSCrypto::OTPStreamCipherReceiver<MType, MSize, CType, CSize>::resetStreamT
     this->prng.revert();
     this->streamByteLocation = this->lastStreamByteLocation;
     this->lastStreamByteLocation = this->streamByteLocation;
-    // Serial.print("resetting sbl: ");
-    // Serial.println(this->streamByteLocation);
 }
 #endif
